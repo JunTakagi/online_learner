0422e1159f4dbf8eb9688e353bc3d15bd4599c8f
jtakagicorp <jtakagi_corp@yahoo.co.jp>
2014-09-21 15:41:59 +0900

    modified LearnerFatory

--- src/main/java/jtakagi/online/learner/LearnerFactory.java
+++ src/main/java/jtakagi/online/learner/LearnerFactory.java
@@ -1,7 +1,13 @@
 package jtakagi.online.learner;
 
+import java.lang.reflect.Constructor;
+
 public class LearnerFactory {
-  public static Learner get(String type) {
-    return new PassiveAggressive(130);
+  @SuppressWarnings("unchecked")
+  public static Learner get(String type) throws Exception {
+    ClassLoader loader = ClassLoader.getSystemClassLoader();
+    Class clazz = loader.loadClass(type);
+    Constructor constructor = clazz.getConstructor(int.class);
+    return (Learner) constructor.newInstance(130);
   }
 }
